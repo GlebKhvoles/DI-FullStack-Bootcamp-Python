4547134c9bcd4061ecf244fab1ffa749fd32510c
Gleb Khvoles <khvoles16@gmail.com>
2023-02-10 19:15:48 +0200

    Add files via upload

--- Week-6/Day-4/Daily-Challenge/Daily-Challenge.py
+++ Week-6/Day-4/Daily-Challenge/Daily-Challenge.py
@@ -1,1 +1,21 @@
+#Exercise1
+number = int(input('Enter a number: '))
+length = int(input('Enter a length: '))
+new_list = []
+if length > 0:
+    for i in range(1, length):
+        new_list.append(number * i)
+    print(new_list)
+else:
+    print('Please enter length more than 0')
 
+#Exercise2
+string = input('Write a string with duplicate consecutive letters: ')
+string_arr = list(string)
+string_arr2 = []
+
+for el in string_arr:
+    if len(string_arr2) == 0 or string_arr2[-1] != el:
+        string_arr2.append(el)
+
+print("".join(string_arr2))
