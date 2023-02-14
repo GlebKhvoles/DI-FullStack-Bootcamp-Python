b7e20c78d85d0b4117095bff3b39658264b3c317
Gleb Khvoles <khvoles16@gmail.com>
2023-02-14 23:34:17 +0200

    Add files via upload

--- Week-7/Day-2/Daily-Challenge/Daily-Challenge.py
+++ Week-7/Day-2/Daily-Challenge/Daily-Challenge.py
@@ -1,1 +1,20 @@
+matrix = [
+	['7', 'i', '3'],
+    ['T', 's', 'i'],
+    ['h', '%', 'x'],
+	['i', ' ', '#'],
+    ['s', 'M', ' '],
+    ['$', 'a', ' '],
+    ['#', 't', '%'],
+    ['^', 'r', '!'],
+]
 
+final_sentence = []
+for column in range(len(matrix[0])):
+	for row in range(len(matrix)):
+		if matrix[row][column].isalpha():
+			final_sentence.append(matrix[row][column])
+		elif len(final_sentence) > 0 and final_sentence[-1] != " " and row > 0:
+			final_sentence.append(" ")
+
+print(''.join(final_sentence))
