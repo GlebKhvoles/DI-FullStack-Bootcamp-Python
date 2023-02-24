4e6e1dd358fd4ea68f78dd677c5a9f2099fecc1c
Gleb Khvoles <khvoles16@gmail.com>
2023-02-24 18:00:44 +0200

    Add files via upload

---
+++ Week-8/Day-4/Daily-Challenge/Pagination.py
@@ -0,0 +1,49 @@
+class Pagination:
+	def __init__(self, items=[], page_size=10):
+		self.items = items
+		self.page_size = int(page_size)
+		self.pages = {}
+		page_slice = slice(int(page_size))
+		i = 1
+		while len(self.items) > 0:
+			self.pages[i] = items[page_slice]
+			del items[0:int(page_size)]
+			i += 1
+		self.total_pages = len(self.pages)
+		self.current_page = 1
+	def get_visible_items(self):
+		return self.pages[self.current_page]
+	def prev_page(self):
+		self.current_page -= 1
+		if self.current_page < 1:
+			self.current_page = 1
+		return self
+	def next_page(self):
+		self.current_page += 1
+		if self.current_page > self.total_pages:
+			self.current_page = self.total_pages
+		return self
+	def first_page(self):
+		self.current_page = 1
+		return self
+	def last_page(self):
+		self.current_page = self.total_pages
+		return self
+	def go_to_page(self, page_num):
+		self.current_page = int(page_num)
+		if self.current_page > self.total_pages:
+			self.current_page = self.total_pages
+		if self.current_page < 1:
+			self.current_page = 1
+		return self
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+
+p = Pagination(alphabetList, 4)
+p.current_page
+print(p.get_visible_items())
+p.next_page().next_page()
+print(p.get_visible_items())
+p.last_page()
+print(p.get_visible_items())
+
