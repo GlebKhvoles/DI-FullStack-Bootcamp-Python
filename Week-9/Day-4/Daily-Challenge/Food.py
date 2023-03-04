9d3dead8d467518558fc3c0d8c97168f120c1f0f
Gleb Khvoles <khvoles16@gmail.com>
2023-03-04 15:27:47 +0200

    Add files via upload

--- Week-9/Day-4/Daily-Challenge/Food.py
+++ Week-9/Day-4/Daily-Challenge/Food.py
@@ -1,1 +1,19 @@
+class Person:
+	def __init__(self, name, like: list, hate: list):
+		self.name = name
+		self.like = like
+		self.hate = hate
 
+	def taste(self, food: str):
+		if food in self.like:
+			food += ' and loves it!'
+		if food in self.hate:
+			food += ' and hates it!'
+		else:
+			food += '!'
+		return f'{self.name} eats the {food}'
+
+p1 = Person('Gleb', ['sushi'], ['holodets'])
+print(p1.taste('sushi'))
+print(p1.taste('holodets'))
+print(p1.taste('pasta'))
