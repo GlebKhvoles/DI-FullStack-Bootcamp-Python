03e79f0f2acdbad3d24dfccc0b3213ee701203cb
Gleb Khvoles <khvoles16@gmail.com>
2023-03-09 21:10:27 +0200

    Delete Translator.py

--- Week-10/Day-2/ExerciseXP/Translator.py
+++
@@ -1,8 +0,0 @@
-from googletrans import Translator
-translator = Translator()
-french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
-result = {}
-for word in french_words:
-    result[word] = translator.translate(word, src='fr').text
-
-print(result)
