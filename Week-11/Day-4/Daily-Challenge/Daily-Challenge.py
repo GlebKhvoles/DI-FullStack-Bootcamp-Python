84418bab348c54840d22c8cdbf37b0f4c239eaaa
Gleb Khvoles <khvoles16@gmail.com>
2023-03-15 23:33:45 +0200

    Add files via upload

--- Week-11/Day-4/Daily-Challenge/Daily-Challenge.py
+++ Week-11/Day-4/Daily-Challenge/Daily-Challenge.py
@@ -1,1 +1,14 @@
+import flask
 
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+	return flask.render_template('homepage.html')
+
+@app.route('/<color>')
+def color_page(color):
+	return flask.render_template('color.html', color=color)
+
+app.run()
