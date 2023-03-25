71d97514a45b1b604dbc06ed79349db2041dc79e
Gleb Khvoles <khvoles16@gmail.com>
2023-03-25 21:04:29 +0300

    Add files via upload

--- Week-12/Day-5/Mini-project/Daily-Challenge/SQL_Puzzle.sql
+++ Week-12/Day-5/Mini-project/Daily-Challenge/SQL_Puzzle.sql
@@ -33,6 +33,6 @@
 -- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
 -- 0
 
---SELECT COUNT(*) 
---FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
 -- 2
